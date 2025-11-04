79f00750ef52ec9c1e7a4b4fa29b83f3997f08bf
wes383 <chmn3333@gmail.com>
2025-11-04 19:31:04 +0800

    fix config

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,7 @@ export default defineConfig(({ mode }) => {
         host: '0.0.0.0',
       },
       plugins: [react()],
+      base: '/20-questions/',
       define: {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
         'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
